b09883b057b8a6c14c3aedf99a25bffb39e1f2b5
Igor Julliano <igor.julliano2@gmail.com>
2023-10-17 00:31:05 -0300

    chore: sort dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
 
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
@@ -21,22 +21,28 @@ sourceSets.main {
 	resources.srcDir("resources")
 }
 
-repositories {
-	mavenCentral()
-}
-
 dependencies {
+	// Data
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("com.auth0:java-jwt:4.4.0")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	implementation("org.flywaydb:flyway-core:9.22.1")
 	runtimeOnly("org.postgresql:postgresql")
+
+	// Security
+	implementation("com.auth0:java-jwt:4.4.0")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
+
+	// Web
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Dev needs
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+//	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+	// Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
