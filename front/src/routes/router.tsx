e6e4dbca0c3802492b257423ef871a9702b6e045
Kelson Eduardo <kfeduardo2003@gmail.com>
2023-10-18 23:30:35 -0300

    feat: added generic routes, you are able to choose what is the type of your route and it will be created

--- front/src/routes/router.tsx
+++ front/src/routes/router.tsx
@@ -1,20 +1,34 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { HomePage } from '../pages/HomePage';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { LoginPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
+import { AnonymusRoute } from "./AnonymusRoute";
+import { ProtectedRoute } from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomePage />,
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
+    ),
     children: [],
   },
   {
-    path: 'register',
-    element: <RegisterPage />,
+    path: "register",
+    element: (
+      <AnonymusRoute>
+        <RegisterPage />
+      </AnonymusRoute>
+    ),
   },
   {
-     path: 'login',
-     element: <LoginPage />,
+    path: "login",
+    element: (
+      <AnonymusRoute>
+        <LoginPage />
+      </AnonymusRoute>
+    ),
   },
 ]);
