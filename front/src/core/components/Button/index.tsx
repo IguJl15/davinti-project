c39f9a5a2697889becaa5f618791c420fd87e958
Igor Julliano <igor.julliano2@gmail.com>
2023-11-21 04:09:15 -0300

    feat: create button component and add material 3 theme css tokens

--- front/src/core/components/Button/index.tsx
+++ front/src/core/components/Button/index.tsx
@@ -1,15 +1,46 @@
-import style from "./style.module.css";
+import { ReactNode } from 'react';
+import styles from './style.module.css';
 
-interface ButtonProps {
+type ButtonStyle = 'Primary' | 'Text' | 'Tonal';
+
+type ButtonProps = {
   label: string;
-}
+  onClick?: () => void;
+  type?: 'button' | 'reset' | 'submit';
+  style?: ButtonStyle;
+  icon?: ReactNode;
+  disabled?: boolean;
+};
 
-function Button({ label }: ButtonProps) {
+// ${styles.disabled}
+
+function PrimaryButton({
+  label,
+  onClick,
+  icon = undefined,
+  type = 'button',
+  style = 'Primary',
+  disabled = false,
+}: ButtonProps) {
   return (
-    <div className={style.button_wrapper}>
-      <input type="submit" value={label} />
-    </div>
+    <button
+      onClick={type == 'button' ? onClick : undefined}
+      onSubmit={type == 'submit' ? onClick : undefined}
+      className={
+        `label-large ` +
+        `${styles.button} ` +
+        `${styles[style]} ` +
+        `${disabled && styles.disabled} ` +
+        `${icon && styles.icon} `
+      }
+      type={type}
+    >
+      <span className={styles.state}>
+        {icon}
+        {label}
+      </span>
+    </button>
   );
 }
 
-export { Button };
+export { PrimaryButton };
