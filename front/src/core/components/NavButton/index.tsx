dc34935e9f6431c45b8d16791008fbd89c0cb3a2
Igor Julliano <igor.julliano2@gmail.com>
2023-11-21 14:05:13 -0300

    refactor: add anonymous routes

--- front/src/core/components/NavButton/index.tsx
+++ front/src/core/components/NavButton/index.tsx
@@ -2,14 +2,16 @@ import { NavLink } from 'react-router-dom';
 import style from './style.module.css';
 
 interface NavButtonProps {
+  label?: string;
   redirectLink: string;
+  onClick?: () => void;
 }
 
-function NavButton({ redirectLink }: NavButtonProps) {
+function NavButton({ label, redirectLink, onClick }: NavButtonProps) {
   return (
     <div className={style.button_wrapper}>
-      <NavLink className={style.nav_link} to={redirectLink}>
-        {redirectLink}
+      <NavLink className={style.nav_link} to={redirectLink} onClick={onClick}>
+        {label ?? redirectLink}
       </NavLink>
     </div>
   );
