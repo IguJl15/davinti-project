73dd6e030a9a974637ae582d3cd499a61fb013f2
Kaike <kaikedias98@gmail.com>
2023-11-20 21:19:29 -0300

    feat: setup authorization on swagger

---
+++ backend/src/config/SwaggerConfig.kt
@@ -0,0 +1,30 @@
+package com.davintiproject.backend.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI().addSecurityItem(SecurityRequirement().addList("Bearer Authentication"))
+            .components(Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+    }
+
+    // Se necessário, adicione métodos auxiliares ou outras configurações aqui.
+
+    private fun createAPIKeyScheme(): SecurityScheme {
+        return SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer")
+    }
+
+}
